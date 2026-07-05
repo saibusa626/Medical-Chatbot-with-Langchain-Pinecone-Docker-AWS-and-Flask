505a7d9b92bad7405439fdfb2cef7477dd5f4fc6
saibusa626 <saibusa626@gmail.com>
2026-07-05 13:32:40 +0200

    new requirements

--- template.sh
+++ template.sh
@@ -4,8 +4,8 @@ mkdir -p research
 touch src/__init__.py
 touch src/helpers.py
 touch src/prompt.py
-touch setup.py
 touch .env
+touch setup.py
 touch app.py
 touch research/trials.ipynb
 touch requirements.txt
